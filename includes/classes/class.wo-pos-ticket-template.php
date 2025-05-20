cd6b8b943eba6c9ed6288250dba43c697d49cb85
Distool <info@distool.de>
2025-05-20 22:55:56 +0200

    Fixing Pos template selct Button in edit Product page

--- includes/classes/class.wo-pos-ticket-template.php
+++ includes/classes/class.wo-pos-ticket-template.php
@@ -1,47 +1,93 @@
 <?php
+/**
+ *
+ * WooCommerce Tickera Bridge Extend
+ * Adds a POS ticket template field to WooCommerce products of type Ticket.
+ *
+ * @package WooCommerce_Tickera_Bridge_Extend
+ */
+
+defined( 'ABSPATH' ) || exit;
+
 if ( ! class_exists( 'WooCommerce_Tickera_Bridge_Extend' ) ) {
 
     class WooCommerce_Tickera_Bridge_Extend {
 
+
+        /**
+         * Constructor
+         */
         public function __construct() {
             add_action( 'woocommerce_product_options_general_product_data', array( $this, 'add_pos_ticket_template_field' ) );
             add_action( 'woocommerce_process_product_meta', array( $this, 'save_pos_ticket_template_field' ) );
         }
 
+        /**
+         * Adds the POS ticket template select field to the product data panel.
+         */
         public function add_pos_ticket_template_field() {
-            echo  '<div class="options_group show_if_tc_ticket">' ;
-            woocommerce_wp_select( [
-                'id'          => '_ticket_pos_template',
-                'label'       => __( 'POS Ticket Template', 'woocommerce-tickera-bridge-extend' ),
-                'options'     => $this->get_ticket_templates_array(),
-                'desc_tip'    => 'true',
-                'description' => __( 'Select how the POS ticket will look.', 'woocommerce-tickera-bridge-extend' ),
-            ] );
-            echo  '<div class="options_group"></div>' ;
-        }
+            global $post;
 
-        public function save_pos_ticket_template_field( $post_id ) {
+            $value = get_post_meta( $post->ID, '_ticket_pos_template_id', true );
+            if( empty( $value ) ) $value = '';
 
-            $post_id = (int) $post_id;
-            // Check if product is a ticket
-            $_tc_is_ticket = ( isset( $_POST['_tc_is_ticket'] ) ? 'yes' : 'no' );
+            echo '<div class="options_group">';
             
-            if ( 'yes' == $_tc_is_ticket ) {
-                // Save choosen ticket template
-                $ticket_pos_template = isset( $_POST['_ticket_pos_template'] ) ? sanitize_text_field( $_POST['_ticket_pos_template'] ) : '';
-                update_post_meta( $post_id, '_ticket_pos_template', $ticket_pos_template );
-            } else {
-                delete_post_meta( $post_id, '_ticket_pos_template' );
+            woocommerce_wp_select(
+                array(
+                    'id'          => '_ticket_pos_template_id',
+                    'label'       => esc_html__( 'POS Ticket Template', 'woocommerce-tickera-bridge-extend' ),
+                    'options'     => $this->get_ticket_templates_array(),
+                    'desc_tip'    => true,
+                    'description' => esc_html__( 'Select how the POS ticket will look.', 'woocommerce-tickera-bridge-extend' ),
+                    'value'      => $value,
+                )
+            );
+
+            echo '</div>';
+        }
+
+        /**
+         * Saves the POS ticket template field value.
+         *
+         * @param int $post_id The ID of the product.
+         * 
+         */
+        public function save_pos_ticket_template_field($post_id) {
+            $woocommerce_ticket_pos_template_id = $_POST['_ticket_pos_template_id'];
+            if( !empty( $woocommerce_ticket_pos_template_id ) )
+                update_post_meta( $post_id, '_ticket_pos_template_id', esc_attr( $woocommerce_ticket_pos_template_id ) );
+            else {
+                update_post_meta( $post_id, '_ticket_pos_template_id',  '' );
             }
         }
 
-        private function get_ticket_templates_array() {
-            // Assuming tc_get_ticket_templates_array is a function that returns an array of ticket templates
-            if ( function_exists( 'tc_get_ticket_templates_array' ) ) {
-                return tc_get_ticket_templates_array();
-            } else {
-                return array(); // Return an empty array if the function does not exist
+        /**
+         * Retrieves available POS ticket templates.
+         *
+         * @return array List of ticket templates.
+         */
+        public function get_ticket_templates_array() {
+            $templates = [];
+
+            $args = [
+                'post_type'      => 'tc_templates',
+                'post_status'    => 'publish',
+                'orderby'        => 'title',
+                'numberposts'    => -1,
+                'order'          => 'ASC',
+            ];
+
+            $templates[''] = __( 'Select a value', 'woocommerce');
+
+            $tc_templates = get_posts( $args );
+            foreach ($tc_templates as $key => $value) {
+                $templates[$value->ID] = $value->post_title;
             }
+            return $templates;
         }
     }
+
+    // Initialize the class.
+    new WooCommerce_Tickera_Bridge_Extend();
 }
--- tickera-order-ticket-fetcher-addon.php
+++ tickera-order-ticket-fetcher-addon.php
@@ -5,79 +5,94 @@
  * Description: A Tickera addon for querying ticket instances based on order information.
  * Author: Distool.de
  * Author URI: https://github.com/distool-de/
- * Version: 0.9.0
+ * Version: 2.0.0
  * Text Domain: tcotf
- * Update URI: https://github.com/distool-de/Tickera-Order-Ticket-Fetcher-Addon/
+ * Requires at least: 6.4
+ * Requires PHP: 7.4
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
-if ( !defined( 'ABSPATH' ) ) {
-    exit;
-}
-// Exit if accessed directly
+defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
 
-// Add Updater
-include_once( plugin_dir_path( __FILE__ ) . 'includes/classes/class.updater.php' );
-
+// Autoload or require necessary class files.
 require_once plugin_dir_path( __FILE__ ) . 'includes/classes/class.wo-pos-ticket-template.php';
+//require_once plugin_dir_path( __FILE__ ) . 'includes/classes/class.tc-pos-ticket-template.php';
 
-// Instantiate the class
-new WooCommerce_Tickera_Bridge_Extend();
+// Register hook to fetch ticket instances via URL query.
+add_action( 'init', 'tcotf_fetch_ticket_instances_from_order' );
 
-// Include the POS Ticket Template class
-require_once plugin_dir_path(__FILE__) . 'includes/classes/class.tc-pos-ticket-template.php';
-
-$updater = new tcotf_Updater( __FILE__ );
-$updater->set_username( 'distool-de' );
-$updater->set_repository( 'Tickera-Order-Ticket-Fetcher-Addon' );
-$updater->initialize();
-
-// Add a function to init hook
-add_action('init', 'get_ticket_instances_from_orderid');
-
-function get_ticket_instances_from_orderid() {
-    // Get the variables from the URL
-    $order_key = ( isset( $_GET['order_key'] ) ? sanitize_key( $_GET['order_key'] ) : '' );
-    $order_id = ( isset( $_GET['order_id'] ) ? (int) $_GET['order_id'] : '' );
-
-    if ( !empty($order_id) && !empty($order_key)) {
-        $order = new TC_Order( $order_id );
-        $order_status = $order->details->post_status;
-        $order_date = strtotime( $order->details->post_date );
-        $order_modified = strtotime( $order->details->post_modified );
-        $tc_order_date = $order->details->tc_order_date;
-        $alt_paid_date = $order->details->_tc_paid_date;
-        $valid_order_statuses = apply_filters( 'tc_validate_downloadable_ticket_order_status', [ 'order_paid' ], $order );
-
-        if ( in_array( $order_status, $valid_order_statuses ) ) {
-            if ( $order_key == $order_date || $order_key == $order_modified || $order_key == $tc_order_date || $alt_paid_date == $order_key ) {
-                $ticket_instances = get_posts( [
-                    'post_type'   => 'tc_tickets_instances',
-                    'post_status' => 'any',
-                    'post_parent' => $order_id,
-                ] );
-
-                $response = array();
-
-                foreach ( $ticket_instances as $ticket_instance ) {
-                    $ticket_id = $ticket_instance->ID;
-                    $ticket_type_id = get_post_meta( $ticket_id, 'ticket_type_id', true );
-                    $ticket_template_POS = get_post_meta( $ticket_type_id, '_ticket_pos_template', true );
-
-                    $response[] = array(
-                        'ticket_id' => $ticket_id,
-                        'ticket_template_POS' => $ticket_template_POS,
-                        'hash' => wp_hash( $ticket_id . $order_key),
-                    );
-                }
-
-                wp_send_json( array( 'status' => '200', 'data' => $response ) );
-            } else {
-                wp_send_json( array( 'status' => '401' ) );
-            }
-        } else {
-            wp_send_json( array( 'status' => '401' ) );
-        }
+/**
+ * Fetches ticket instances based on the order ID and key.
+ *
+ * @return void
+ */
+function tcotf_fetch_ticket_instances_from_order() {
+    if ( ! isset( $_GET['order_key'], $_GET['order_id'] ) ) {
+        return;
     }
-}
 
-?>
+    $order_key = sanitize_text_field( wp_unslash( $_GET['order_key'] ) );
+    $order_id  = intval( $_GET['order_id'] );
+
+    if ( empty( $order_id ) || empty( $order_key ) ) {
+        return;
+    }
+
+    if ( ! class_exists( 'TC_Order' ) ) {
+        return;
+    }
+
+    $order            = new TC_Order( $order_id );
+    $order_status     = $order->details->post_status ?? '';
+    $order_date       = strtotime( $order->details->post_date ?? '' );
+    $order_modified   = strtotime( $order->details->post_modified ?? '' );
+    $tc_order_date    = $order->details->tc_order_date ?? '';
+    $alt_paid_date    = $order->details->_tc_paid_date ?? '';
+
+    $valid_statuses = apply_filters( 'tc_validate_downloadable_ticket_order_status', [ 'order_paid' ], $order );
+
+    if ( ! in_array( $order_status, $valid_statuses, true ) ) {
+        wp_send_json( [ 'status' => 401, 'message' => 'Invalid order status.', 'data' => [] ] );
+    }
+
+    if (
+        $order_key !== (string) $order_date &&
+        $order_key !== (string) $order_modified &&
+        $order_key !== (string) $tc_order_date &&
+        $order_key !== (string) $alt_paid_date
+    ) {
+        wp_send_json( [ 'status' => 401, 'message' => 'Invalid order key.', 'data' => [] ] );
+    }
+
+    $ticket_instances = get_posts(
+        [
+            'post_type'   => 'tc_tickets_instances',
+            'post_status' => 'any',
+            'post_parent' => $order_id,
+            'numberposts' => -1,
+        ]
+    );
+
+    $response = [];
+
+    foreach ( $ticket_instances as $ticket_instance ) {
+        $ticket_id         = $ticket_instance->ID;
+        $ticket_type_id    = get_post_meta( $ticket_id, 'ticket_type_id', true );
+        $ticket_template   = get_post_meta( $ticket_type_id, '_ticket_pos_template_id', true );
+
+        $response[] = [
+            'ticket_id'          => $ticket_id,
+            'ticket_template_POS'=> $ticket_template,
+            'hash'               => wp_hash( $ticket_id . $order_key ),
+        ];
+    }
+
+    wp_send_json(
+        [
+            'status' => 200,
+            'message' => 'Ticket instances retrieved successfully.',
+            'data'   => $response,
+        ]
+    );
+}
